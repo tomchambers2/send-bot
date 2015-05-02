950ef72f0f0113e11a6393e5fe6b3d194587ca83
Tom Chambers <tom.chambers@gmail.com>
2015-05-02 20:49:19 +0100

    first commit

---
+++ sender.py
@@ -0,0 +1,36 @@
+import mandrill
+
+def sendEmail(name, email, subject, message):
+	try:
+		print "Sending email to %s", email
+		mandrill_client = mandrill.Mandrill('DuIMK8qbJHaZYYAvfET5gQ')
+		message = {
+		 'auto_html': None,
+		 'auto_text': None,
+		 'from_email': email,
+		 'from_name': name,
+		 'headers': {'Reply-To': email},
+		 'metadata': {'website': 'www.example.com'},
+		 'preserve_recipients': None,
+		 'recipient_metadata': [{'rcpt': 'tom.chambers@gmail.com',
+								 'values': {'user_id': 123456}}],
+		 'return_path_domain': None,
+		 'signing_domain': None,
+		 'subject': subject,
+		 'tags': ['password-resets'],
+		 'text': message,
+		 'to': [{'email': 'tom.chambers@gmail.com',
+				 'name': 'London Consent Project',
+				 'type': 'to'}],
+		 'track_clicks': None,
+		 'track_opens': None,
+		 'tracking_domain': None,
+		 'url_strip_qs': None,
+		 'view_content_link': None}
+		result = mandrill_client.messages.send(message=message, async=False, ip_pool='Main Pool')
+
+	except mandrill.Error, e:
+		# Mandrill errors are thrown as exceptions
+		print 'A mandrill error occurred: %s - %s' % (e.__class__, e)
+		# A mandrill error occurred: <class 'mandrill.UnknownSubaccountError'> - No subaccount exists with the id 'customer-123'    
+		raise
---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+from sender import sendEmail
+
+@app.route('/send', methods=['POST'])
+def index():
+	print "Hit send email endpoint"
+	sendEmail(request.form.getlist('name')[0], request.form.getlist('email')[0], request.form.getlist('subject')[0], request.form.getlist('message')[0]+'\n\nSent from London Consent Project contact form')
+	return jsonify({'tasks': 'tits'})
+
+if __name__ == '__main__':
+	app.run(debug=True)
