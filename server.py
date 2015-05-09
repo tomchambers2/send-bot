9499e2d91093f60affc83ac9c3d497313306a5f7
Tom Chambers <tom.chambers@gmail.com>
2015-05-09 11:35:21 +0100

    add access control headers

--- server.py
+++ server.py
@@ -3,8 +3,10 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 from sender import sendEmail
+from access_control import crossdomain
 
 @app.route('/send/london-consent-project', methods=['POST'])
+@crossdomain(origin='*')
 def index():
 	print "Hit send email endpoint"
 	sendEmail(request.form.getlist('name')[0], request.form.getlist('email')[0], request.form.getlist('subject')[0], request.form.getlist('message')[0]+'\n\nSent from London Consent Project contact form')
